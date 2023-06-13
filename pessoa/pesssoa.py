a2da6021238db140d31ecca08c0d422f1ac96eb8
Fábio Correia Lima <118375746+FabioCorreiaLima@users.noreply.github.com>
2023-06-13 12:42:20 -0300

    Update pesssoa.py

--- pessoa/pesssoa.py
+++ pessoa/pesssoa.py
@@ -1,7 +1,6 @@
-class Interessado:
+class Interesso:
     def __init__(self, nome, contato, especie_interesse, preferencia_animal=Non):
         self.nome = nome
         self.contato = contat
         self.especie_interesse = especie_interesse
         self.preferencia_animal = preferencia_animal
-        
