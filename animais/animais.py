ef0c3d70bc41a9e6470cb19cf3be7bcf1f221626
Fábio Correia Lima <118375746+FabioCorreiaLima@users.noreply.github.com>
2023-06-13 12:37:39 -0300

    Update animais.py

---
+++ animais/animais.py
@@ -0,0 +1,7 @@
+class Animal:
+    def __init__(self, tipo, idade, cor, porte, particularidade=None):
+        self.tipo = tipo
+        self.idade = idade
+        self.cor = cor
+        self.porte = porte
+        self.particularidade = particularidade
